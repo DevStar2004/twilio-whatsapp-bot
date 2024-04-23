214f3eaf08bfbfe10365956595cae580f4a5a6e7
DevStar2004 <DevStar2004@outlook.com>
2024-04-24 05:30:14 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('hello');
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/callback", (req:any, res:any) => {
+    res.sendStatus(200)
+})
+
+app.listen(4000, () => {
+    console.log('Application has started.');
+})
+
+app.get("/", (req:any, res:any) => {
+    res.send("Its running")
+});
+
